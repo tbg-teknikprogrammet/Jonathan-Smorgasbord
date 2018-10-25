d37e1e860b0c64b6ceec05deb5e37febf30f4ea4
Jonathan Arnfjell <Jonathan.DTK.Arnfjell@lystkom.se>
2018-10-25 14:03:18 +0200

    init commit

---
+++ projects/javascript101/assets/js/scrpt.js
@@ -0,0 +1,15 @@
+var text = "";
+var i = 300;
+
+// Skriv ut udda tal mellan 300 - 333
+while(i <= 333) {
+    text += "numret är:" + i + "<br>";
+    i++;
+
+    if(i % 2 ==0) {
+        i++;
+    }
+}
+
+document.getElementById("output").innerHTML = text;
+
